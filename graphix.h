03ba670f9e5294ccb1268db0676054cab4b76a59
wvovaw <vladimir5313375015@gmail.com>
2019-09-23 22:48:08 +0500

    init repo

---
+++ graphix.h
@@ -0,0 +1,8 @@
+#ifndef GRAPHIX_H
+#define GRAPHIX_H
+
+#include "tgaimage.h"
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+
+#endif // GRAPHIX_H
---
+++ headers.h
@@ -0,0 +1,16 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "model.h"
+#include "geometry.h"
+#include "graphix.h"
+#include "model.h"
+#include "tgaimage.h"
+
+#endif // HEADERS_H
